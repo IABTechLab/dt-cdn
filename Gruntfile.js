5cb8b4ca1f52f05ac34072630cf63f4aedaf2d95
Sam Tingleff <sam@tingleff.com>
2017-06-05 15:57:43 -0700

    bumped release version to 1.3.1.beta

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
     // Project configuration
     'use strict';
     
-    var digitrustVersion = '1.3.0';
+    var digitrustVersion = '1.3.1.beta';
 
     // Get or Default environment
     var argEnv = grunt.option('env');
