26e8daf9859d941153725ce4409ff56d6a2783f3
Sam Tingleff <sam@tingleff.com>
2017-06-22 13:28:12 -0700

    #32 use Uint8Array and avoid insert duplicate characters

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -297,12 +297,12 @@ helpers.isValidJSON = function (str) {
 };
 
 helpers.ab2str = function (buf) {
-    return String.fromCharCode.apply(null, new Uint16Array(buf));
+    return String.fromCharCode.apply(null, new Uint8Array(buf));
 };
 
 helpers.str2ab = function (str) {
-    var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-    var bufView = new Uint16Array(buf);
+    var buf = new ArrayBuffer(str.length);
+    var bufView = new Uint8Array(buf);
     for (var i = 0, strLen = str.length; i < strLen; i++) {
         bufView[i] = str.charCodeAt(i);
     }
