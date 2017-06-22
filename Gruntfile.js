552de3ca19ef069e26582005c34c9fcbe44ac13a
Sam Tingleff <sam@tingleff.com>
2017-06-22 13:30:41 -0700

    bump version to 1.4.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
     // Project configuration
     'use strict';
     
-    var digitrustVersion = '1.4.0';
+    var digitrustVersion = '1.4.1';
 
     // Get or Default environment
     var argEnv = grunt.option('env');
