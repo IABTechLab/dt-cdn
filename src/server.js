85791eadba44d49ab7581f84a48e984de459b35a
Sam Tingleff <sam@tingleff.com>
2017-06-22 15:28:31 -0700

    #34 removed DigiTrustAppContainer.js

--- src/server.js
+++ src/server.js
@@ -4,14 +4,12 @@ var configGeneral = require('./config/general.json')[env];
 var DigiTrust = require('./modules/DigiTrust');
 DigiTrust.isClient = false;
 var DigiTrustCookie = require('./modules/DigiTrustCookie');
-var DigiTrustAppContainer = require('./modules/DigiTrustAppContainer');
 var DigiTrustCrypto = require('./modules/DigiTrustCrypto');
 var helpers = require('./modules/helpers');
 
 if (window !== undefined) {
     window.DigiTrust = DigiTrust;
     window.DigiTrustCookie = DigiTrustCookie;
-    window.DigiTrustAppContainer = DigiTrustAppContainer;
     window.DigiTrustCrypto = DigiTrustCrypto;
     window.configGeneral = configGeneral;
     window.helpers = helpers;
