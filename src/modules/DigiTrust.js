515d0b1ee4e92678bad0fd094de6d13e44843315
Sam Tingleff <sam@tingleff.com>
2017-06-27 16:47:11 -0700

    remove more adblocker reference

--- src/modules/DigiTrust.js
+++ src/modules/DigiTrust.js
@@ -43,16 +43,6 @@ DigiTrust.initialize = function (options, initializeCallback) {
             return initializeCallback(identityResponseObject);
         }
 
-        if (options.adblocker.unstyled) {
-            var head  = document.getElementsByTagName('head')[0];
-            var link  = document.createElement('link');
-            link.rel  = 'stylesheet';
-            link.type = 'text/css';
-            link.href = options.adblocker.customCssPath;
-            link.media = 'all';
-            head.appendChild(link);
-        }
-
         // Verify Publisher's Member ID
         if (!DigiTrust._isMemberIdValid(options.member)) {
             return initializeCallback(identityResponseObject);
