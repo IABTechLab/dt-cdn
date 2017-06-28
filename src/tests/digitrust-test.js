7f598e2037f77b33fd2901394cc5619711fdef39
Sam Tingleff <sam@tingleff.com>
2017-06-28 15:18:27 -0700

    test for non-zero ID bytes

--- src/tests/digitrust-test.js
+++ src/tests/digitrust-test.js
@@ -219,6 +219,14 @@ describe('helpers', function () {
         var binaryId = helpers.base64StringToArrayBuffer(id);
         expect(binaryId).not.toBe(null);
         expect(binaryId.byteLength).toBe(8);
+        var a = new Uint8Array(binaryId);
+        expect(a.length).toBe(8);
+        // don't blame me that reduce() is not available in PhantomJS
+        var total = 0;
+        for (var i = 0; i < a.length; ++i) {
+            total += a[i];
+        }
+        expect(total).not.toBe(0);
         // convert back to base64 string
         expect(id).toBe(helpers.arrayBufferToBase64String(binaryId));
     });
