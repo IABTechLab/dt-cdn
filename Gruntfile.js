797b51ad5eb0dba086d1744fda3a1dcade9928bb
Sam Tingleff <sam@tingleff.com>
2018-01-04 14:08:41 -0800

    bump version

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
     'use strict';
     
     var digitrustMajorVersion = 'notification-poc';
-    var digitrustVersion = 'notification-poc-1.0.0.beta';
+    var digitrustVersion = 'notification-poc-1.0.1.beta';
 
     // Get or Default environment
     var argEnv = grunt.option('env');
