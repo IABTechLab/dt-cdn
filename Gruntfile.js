5312020b5fbf540ae957cac46351f7c33da1ce60
Sam Tingleff <sam@tingleff.com>
2018-04-25 15:09:16 -0400

    release to 1.5.10.beta

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
     'use strict';
     
     var digitrustMajorVersion = '1';
-    var digitrustVersion = '1.5.9';
+    var digitrustVersion = '1.5.10.beta';
 
     // Get or Default environment
     var argEnv = grunt.option('env');
