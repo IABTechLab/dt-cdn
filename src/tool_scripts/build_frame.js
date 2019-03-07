b0367ca2c32f5d6891ec0c5160c6956646d9fc01
Chris Cole <goosemanjack@gmail.com>
2019-03-07 09:44:47 -0800

    DIS-38 Build target "buildAll" creates both normal and debug target iFrame files.

--- src/tool_scripts/build_frame.js
+++ src/tool_scripts/build_frame.js
@@ -5,45 +5,105 @@
 var fs = require('fs');
 
 var markupBuffer = null,
-  scriptBuffer = null;
+  iframeBuffer = null,
+  scriptFullBuffer = null,
+  scriptMinBuffer = null;
 
-const EMBED_COMMENT = '/* SCRIPT CONTENTS HERE */';
-const OUTPUT_FILE = 'dt-embed.html';
+const DEBUG_IDAPI_SETTING = 'configGeneral.urls.digitrustIdService = "../misc/faked_id_service_v1.json" //http://local.digitru.st/misc/faked_id_service_v1.json'
+const DEBUG_SCRIPT_TAG = "<script src='./digitrust_iframe.js'></script>"
+const PROD_SCRIPT_TAG = "<script src='./digitrust_iframe.min.js'></script>"
+
+const outputFile = {
+  EMBEDDED: 'dt-embed.html',
+  REF: 'dt.html',
+  DEBUG: 'dt_debug.html'
+};
+
+const replaceTokens = {
+  SCRIPTREF: "<script src='./digitrust.min.js'></script>",
+  CONFIG_DEBUG: '/* TOKEN ADDITIONAL SETTINGS */'
+}
 
 
 fs.readFile('./dist/digitrust_iframe.min.js', 'utf8', function (err, contents) {
-  scriptBuffer = contents;
-  checkComplete(scriptBuffer, markupBuffer);
+  scriptMinBuffer = contents;
+  checkComplete(scriptMinBuffer, scriptFullBuffer, iframeBuffer);
 });
-fs.readFile('./pages/dt_debug.html', 'utf8', function (err, contents) {
-  markupBuffer = contents;
-  checkComplete(scriptBuffer, markupBuffer);
+fs.readFile('./dist/digitrust_iframe.js', 'utf8', function (err, contents) {
+  scriptFullBuffer = contents;
+  checkComplete(scriptMinBuffer, scriptFullBuffer, iframeBuffer);
+});
+fs.readFile('./pages/dt.html', 'utf8', function (err, contents) {
+  iframeBuffer = contents;
+  checkComplete(scriptMinBuffer, scriptFullBuffer, iframeBuffer);
 });
 
 
-
-function checkComplete(scripts, markup){
-  if (scripts == null || markup == null) {
+/**
+ * Tests that all needed contents are loaded.
+ * If so, proceeds with writing the files
+ * @param {any} scripts
+ * @param {any} markup
+ */
+function checkComplete(scriptMin, scriptFull, markup){
+  if (scriptMin == null
+    || scriptFull == null
+    || markup == null) {
     return;
   }
 
-  let pos = markup.indexOf(EMBED_COMMENT);
-  var newData = markup.replace(EMBED_COMMENT, scripts);
+  var debugMarkup = formatDebugFile(markup);
+  var embedMarkup = formatEmbedFile(markup, scriptMin);
+  var refMarkup = formatScriptRef(markup);
+  var fileRef;
+  
+  var files = [
+    { file: './dist/' + outputFile.REF, data: refMarkup },
+    { file: './dist/' + outputFile.EMBEDDED, data: embedMarkup },
+    { file: './dist/' + outputFile.DEBUG, data: debugMarkup }
+  ];
 
-  var outputFile = './dist/' + OUTPUT_FILE;
-  if (fs.existsSync(outputFile)) {
-    fs.unlinkSync(outputFile);
-  }
 
-  fs.writeFile(outputFile, newData, function (err, data) {
-    if (err) {
-      console.log(err);
-      return;
+  for (let i = 0; i < files.length; i++) {
+    fileRef = files[i].file;
+    if (fs.existsSync(fileRef)) {
+      fs.unlinkSync(fileRef);
     }
-    console.log("Successfully Written to File.");
-  });
 
+    fs.writeFile(fileRef, files[i].data, function (err, data) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    });
+  }
 }
 
-// console.log(scriptBuffer);
+
+/**
+ * Format the source into the debug iFrame
+ * @param {any} markup
+ */
+function formatDebugFile(markup) {
+  var result = markup.replace(replaceTokens.SCRIPTREF, DEBUG_SCRIPT_TAG).replace(replaceTokens.CONFIG_DEBUG, DEBUG_IDAPI_SETTING);
+  return result;
+}
+
+/**
+ * Format the source into the debug iFrame
+ * @param {any} markup
+ */
+function formatEmbedFile(markup, embedScript) {
+  var result = markup.replace(replaceTokens.SCRIPTREF, "<script>\n" + embedScript + "\n</script>\n");
+  return result;
+}
+
+/**
+ * Format the source into the debug iFrame
+ * @param {any} markup
+ */
+function formatScriptRef(markup) {
+  var result = markup.replace(replaceTokens.SCRIPTREF, PROD_SCRIPT_TAG);
+  return result;
+}
 
